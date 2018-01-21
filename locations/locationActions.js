4ed0c7ae29d4c414df5ac0f11753d69d18d44e4b
Trenton w Fleming <trenflem@gmail.com>
2018-01-21 03:37:48 -0500

    updated locations response

--- locations/locationActions.js
+++ locations/locationActions.js
@@ -3,7 +3,7 @@ const Location = require('./locationsModel.js');
 module.exports = {
     get: async function(req, res) {
         let locations = await Location.find();
-
-        res.status(200).send(locations);
+        console.log(locations)
+        res.status(200).send({ localLocations: locations });
     }
 }
